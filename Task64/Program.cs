c7f40c1ca33098eb35fd2ccefb84184caa75c1a5
Ksenia Tovmasyan <ksenia.tovmasyan@yandex.ru>
2022-09-10 16:02:48 +0700

    Task 64 done

---
+++ Task64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+void ShowNumsNM(int n, int m)
+{
+    if (m > n)
+    {
+        int temp = n;
+        n = m;
+        m = temp;
+    }
+    if (n != m) ShowNumsNM(n - 1, m);
+    Console.Write(n + " ");
+}
+Console.Write("Input number M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+ShowNumsNM(numN, numM);
